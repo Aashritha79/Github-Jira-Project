9011b539c7577f077c0e2655d27c65a558665d0b
Aashritha Dodda <123049325+Aashritha79@users.noreply.github.com>
2024-07-27 04:40:55 +0000

    update all files

---
+++ github-jira/github-jira.project/create-jira.py
@@ -0,0 +1,53 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import os
+
+url = "https://aashrithadodda79.atlassian.net/rest/api/3/issue"
+
+token = os.getenv("API_TOKEN")
+
+auth = HTTPBasicAuth("aashritha.dodda79@gmail.com", token)
+
+headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json"
+}
+
+payload = json.dumps( {
+  "fields": {
+    "description": {
+      "content": [
+        {
+          "content": [
+            {
+              "text": "My first jira ticket",
+              "type": "text"
+            }
+          ],
+          "type": "paragraph"
+        }
+      ],
+      "type": "doc",
+      "version": 1
+    },
+    "project": {
+      "key": "GJP"
+    },
+    "issuetype": {
+      "id": "10003"
+    },
+    "summary": "Sixth JIRA Ticket",
+  },
+  "update": {}
+} )
+
+response = requests.request(
+   "POST",
+   url,
+   data=payload,
+   headers=headers,
+   auth=auth
+)
+
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
